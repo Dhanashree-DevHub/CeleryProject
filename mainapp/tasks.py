0ddae20dccc962478740cdb0e2efb367d5d2d572
Dhanashree-DevHub <ranadedhanu16@gmail.com>
2025-08-20 23:46:34 +0530

    Add new shared task

---
+++ mainapp/tasks.py
@@ -0,0 +1,16 @@
+from celery import shared_task
+from .models import Report
+import time
+import logging
+
+logger = logging.getLogger(__name__)
+
+@shared_task(name="dummy_and_slow")
+def dummy_and_slow():
+    time.sleep(2)
+    logger.debug("Dummy and slow task has finished")
+    
+@shared_task(name="mainapp_generate_report")
+def generate_report(report_id):
+    report = Report.objects.get(id=report_id)
+    report.generate()
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,38 @@
 from django.shortcuts import render
-
+from .tasks import dummy_and_slow 
+import time
+from django.views.generic.edit import CreateView
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+from django.urls import reverse  # Add this line
+from .models import Report
 def index(request):
     return render(request, 'mainapp/index.html')
 
+def dummy_and_slow_view(request):
+    start_time = time.time()
+    dummy_and_slow.delay()
+    end_time=time.time()
+    execution_time = end_time - start_time
+    context = {"task_name": "Dummy and slow", "execution_time": round(execution_time, 2)}
+    return render(request, 'mainapp/generic.html', context=context)
+
 # Create your views here.
+class ReportCreateView(CreateView):
+    model = Report
+    fields = ['complexity']
+    template_name = 'mainapp/report_form.html'
+    
+    def get_success_url(self):
+        return reverse('report_detail', kwargs={'pk': self.object.pk})
+class ReportDetailView(DetailView):
+    model = Report
+    context_object_name = 'report'
+    template_name = 'mainapp/report_detail.html'
+    
+class ReportListView(ListView):
+    model = Report
+    template_name = "mainapp/report_list.html"
+    context_object_name = "reports"
+    ordering = ["-dt_created"]
+    paginate_by = 10
